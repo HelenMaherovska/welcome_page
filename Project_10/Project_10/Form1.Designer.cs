6e1b397b29f919808162b3fab63f2146eead729e
hmaherovska <maher.olena96@gmail.com>
2017-05-31 17:31:05 +0300

    added welcome page

---
+++ Project_10/Project_10/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Project_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Download_btn = new System.Windows.Forms.Button();
+            this.Save_btn = new System.Windows.Forms.Button();
+            this.Sort_btn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Search = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(649, 436);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Download_btn
+            // 
+            this.Download_btn.Location = new System.Drawing.Point(690, 12);
+            this.Download_btn.Name = "Download_btn";
+            this.Download_btn.Size = new System.Drawing.Size(84, 51);
+            this.Download_btn.TabIndex = 1;
+            this.Download_btn.Text = "Download";
+            this.Download_btn.UseVisualStyleBackColor = true;
+            this.Download_btn.Click += new System.EventHandler(this.Download_btn_Click);
+            // 
+            // Save_btn
+            // 
+            this.Save_btn.Location = new System.Drawing.Point(690, 87);
+            this.Save_btn.Name = "Save_btn";
+            this.Save_btn.Size = new System.Drawing.Size(84, 51);
+            this.Save_btn.TabIndex = 2;
+            this.Save_btn.Text = "Save";
+            this.Save_btn.UseVisualStyleBackColor = true;
+            this.Save_btn.Click += new System.EventHandler(this.Save_btn_Click);
+            // 
+            // Sort_btn
+            // 
+            this.Sort_btn.Location = new System.Drawing.Point(690, 166);
+            this.Sort_btn.Name = "Sort_btn";
+            this.Sort_btn.Size = new System.Drawing.Size(84, 51);
+            this.Sort_btn.TabIndex = 3;
+            this.Sort_btn.Text = "Sort";
+            this.Sort_btn.UseVisualStyleBackColor = true;
+            this.Sort_btn.Click += new System.EventHandler(this.Sort_btn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(674, 250);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(690, 290);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(75, 23);
+            this.Search.TabIndex = 5;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 460);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Sort_btn);
+            this.Controls.Add(this.Save_btn);
+            this.Controls.Add(this.Download_btn);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Download_btn;
+        private System.Windows.Forms.Button Save_btn;
+        private System.Windows.Forms.Button Sort_btn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Search;
+    }
+}
+
---
+++ Project_10/Project_10/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Project_10
+{
+    public partial class Form1 : Form
+    {
+        StreamReader rd = new StreamReader(@"C:\Users\ostap\Desktop\C# Programs 2-nd year\Project_10\Project_10\Text_test.txt");
+        StreamWriter wr = new StreamWriter(@"C:\Users\ostap\Desktop\C# Programs 2-nd year\Project_10\Project_10\ResultFile.txt", false);
+        DataSet ds = new DataSet();
+
+        public Form1()
+        {
+           InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Download_btn_Click(object sender, EventArgs e)
+        {
+            ds.Tables.Add("Result");
+            string header = rd.ReadLine();
+            string[] number = System.Text.RegularExpressions.Regex.Split(header,",");
+            
+            for(int i=0;i<number.Length;i++)
+            {
+                ds.Tables[0].Columns.Add(number[i]);
+            }
+
+            string row = rd.ReadLine();
+
+            while(row!=null)
+            {
+                string[] rvalue = System.Text.RegularExpressions.Regex.Split(row, ",");
+                ds.Tables[0].Rows.Add(rvalue);
+                row = rd.ReadLine();
+            }
+
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void Save_btn_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                wr.WriteLine((ds.Tables[0].Rows[i][0]).ToString() + "\t" + (ds.Tables[0].Rows[i][1]).ToString() + "\t" + (ds.Tables[0].Rows[i][2]).ToString() + "\t" + (ds.Tables[0].Rows[i][3]).ToString());
+            }
+
+            wr.Close();
+
+            //SaveFileDialog save = new SaveFileDialog();
+
+            //save.Title = "Save File";
+            //save.Filter = "Text file (*.txt)|*.txt|All Files(*.*)|*.*";
+
+            //if (save.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            //{
+            //    StreamWriter write = new StreamWriter(File.Create(save.FileName));
+            //    save.FileName = @"C:\Users\ostap\Desktop\C# Programs 2-nd year\Project_10\Project_10\ResultFile.txt";
+            //    //write.Write(txt_Area.Text);
+            //    write.Dispose();
+            //} 
+        }
+
+        private void Sort_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Search_Click(object sender, EventArgs e)
+        {
+            //bool r = false;
+            //if (dataGridView1.RowCount != 0)
+            //    if (textBox1.Text == "")
+            //    {
+            //        MessageBox.Show("You Didn't Input any data!!!");
+            //    }
+            //    else
+            //    {
+            //        for (int i = 0; i < dataGridView1.RowCount; i++)
+            //        {
+            //            if (string.Compare((ds.Tables[0].Rows[i][1]).ToString(), textBox1.Text, true) == 0)
+            //            {
+            //                r = true;
+            //                string s = ds.Tables[0].Rows[i][0].ToString() + " " + ds.Tables[0].Rows[i][1].ToString() + " " + ds.Tables[0].Rows[i][2].ToString() + " " + ds.Tables[0].Rows[i][3].ToString();
+            //                MessageBox.Show(s);
+            //                break;
+            //            }
+
+            //        }
+            //        if (!r)
+            //        {
+            //            MessageBox.Show("Didn't find anything!!!");
+            //        }
+            //    }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
